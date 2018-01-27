b8ff9bb4db75b96017a931f7231e3eeec8ea75be
naomi19 <31074228+naomi19@users.noreply.github.com>
2018-01-27 15:19:07 -0500

    lab

---
+++ Activities/Labs/Lab #6/lab6.js
@@ -0,0 +1,20 @@
+window.onload = function()
+{
+    var randomBox1 = document.getElementById('eventBox1');
+    var randomBox2 = document.getElementById('eventBox2');
+    
+    randomBox2.onmouseover = function()
+    {
+        alert('You hovered over the red box!');
+    };
+    
+    randomBox1.onclick = function(e)
+    {
+        alert('X=' + e.clientX + 'and Y=' + e.clientY)
+    }
+    
+    window.onkeypress = function()
+    {
+        alert('You pressed a key!')
+    }
+}
